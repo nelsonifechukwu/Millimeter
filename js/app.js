d6f7bb28aa7be79605e4bd5e43610321916263f7
Viqueer <thethinkerx100@gmail.com>
2020-09-03 03:33:00 +0100

    Improved the graph

--- js/app.js
+++ js/app.js
@@ -167,3 +167,235 @@ if (addPaymentModalCloseBtn) {
 		close(addPaymentModal);
 	});
 }
+
+
+
+
+//CHART JS
+let demo = {
+	initDocChart: function () {
+		chartColor = "#FFFFFF";
+
+		// General configuration for the charts with Line gradientStroke
+		gradientChartOptionsConfiguration = {
+			maintainAspectRatio: false,
+			legend: {
+				display: false
+			},
+			tooltips: {
+				bodySpacing: 4,
+				mode: "nearest",
+				intersect: 0,
+				position: "nearest",
+				xPadding: 10,
+				yPadding: 10,
+				caretPadding: 10
+			},
+			responsive: true,
+			scales: {
+				yAxes: [{
+					display: 0,
+					gridLines: 0,
+					ticks: {
+						display: false
+					},
+					gridLines: {
+						zeroLineColor: "transparent",
+						drawTicks: false,
+						display: false,
+						drawBorder: false
+					}
+				}],
+				xAxes: [{
+					display: 0,
+					gridLines: 0,
+					ticks: {
+						display: false
+					},
+					gridLines: {
+						zeroLineColor: "transparent",
+						drawTicks: false,
+						display: false,
+						drawBorder: false
+					}
+				}]
+			},
+			layout: {
+				padding: {
+					left: 0,
+					right: 0,
+					top: 15,
+					bottom: 15
+				}
+			}
+		};
+
+		ctx = document.getElementById('lineChartExample').getContext("2d");
+
+		gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+		gradientStroke.addColorStop(0, '#80b6f4');
+		gradientStroke.addColorStop(1, chartColor);
+
+		gradientFill = ctx.createLinearGradient(0, 170, 0, 50);
+		gradientFill.addColorStop(0, "rgba(128, 182, 244, 0)");
+		gradientFill.addColorStop(1, "rgba(249, 99, 59, 0.40)");
+
+		myChart = new Chart(ctx, {
+			type: 'line',
+			responsive: true,
+			data: {
+				labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+				datasets: [{
+					label: "Active Users",
+					borderColor: "#f96332",
+					pointBorderColor: "#FFF",
+					pointBackgroundColor: "#f96332",
+					pointBorderWidth: 2,
+					pointHoverRadius: 4,
+					pointHoverBorderWidth: 1,
+					pointRadius: 4,
+					fill: true,
+					backgroundColor: gradientFill,
+					borderWidth: 2,
+					data: [542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 630]
+				}]
+			},
+			options: gradientChartOptionsConfiguration
+		});
+	},
+}
+
+demo.initDashboardPageCharts = function () {
+
+	gradientChartOptionsConfigurationWithTooltipOrange = {
+		maintainAspectRatio: false,
+		legend: {
+			display: false
+		},
+
+		tooltips: {
+			backgroundColor: '#f5f5f5',
+			titleFontColor: '#333',
+			bodyFontColor: '#666',
+			bodySpacing: 4,
+			xPadding: 12,
+			mode: "nearest",
+			intersect: 0,
+			position: "nearest"
+		},
+		responsive: true,
+		scales: {
+			yAxes: [{
+				barPercentage: 1.6,
+				gridLines: {
+					drawBorder: false,
+					color: 'rgba(29,29,29,0.0)',
+					zeroLineColor: "transparent",
+				},
+				ticks: {
+					suggestedMin: 0, //minimum value for the Y axis of this format of the chart
+					suggestedMax: 2000, //maximum value for the Y axis of this format of the chart
+					padding: 20,
+					fontColor: "#999A9F"
+				}
+			}],
+
+			xAxes: [{
+				barPercentage: 1.6,
+				gridLines: {
+					drawBorder: false,
+					color: 'rgba(220,53,69,0.1)',
+					zeroLineColor: "transparent",
+				},
+				ticks: {
+					padding: 20,
+					fontColor: "#999A9F"
+				}
+			}]
+		}
+	};
+
+	
+
+
+
+	//LIGHT INTENSITY CHART
+	var ctx = document.getElementById("energyConsumptionChart").getContext("2d");
+
+	var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+
+	gradientStroke.addColorStop(1, 'rgba(255, 81, 25,0.5)');
+	gradientStroke.addColorStop(0.2, 'rgba(255, 100, 145,0.0)');
+	gradientStroke.addColorStop(0, 'rgba(255, 100, 145,0)'); //purple colors
+
+	var data = {
+		labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov', 'Dec'], //LABELS
+		datasets: [{
+			label: "Data",
+			fill: true,
+			backgroundColor: gradientStroke,
+			borderColor: '#ff8d72',
+			borderWidth: 2,
+			borderDash: [],
+			borderDashOffset: 0.0,
+			pointBackgroundColor: '#ff8d72',
+			pointBorderColor: 'rgba(255,255,255,0)',
+			pointHoverBackgroundColor: '#d048b6',
+			pointBorderWidth: 20,
+			pointHoverRadius: 4,
+			pointHoverBorderWidth: 15,
+			pointRadius: 4,
+			data: [500,905,300,1500,400,1203,1400,350,700,1400,900,1500] //CHART DATA
+		}]
+	};
+
+	var myChart = new Chart(ctx, {
+		type: 'line',
+		data: data,
+		options: gradientChartOptionsConfigurationWithTooltipOrange //CHART FORMAT
+	});
+
+
+
+
+	var ctx = document.getElementById("energyConsumptionChart2").getContext("2d");
+
+	var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+
+	gradientStroke.addColorStop(1, 'rgba(255, 81, 25,0.5)');
+	gradientStroke.addColorStop(0.2, 'rgba(255, 81, 25,0.3)');
+	gradientStroke.addColorStop(0, 'rgba(255, 100, 145,0)'); //purple colors
+
+	var data = {
+		labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], //LABELS
+		datasets: [{
+			label: "Data",
+			fill: true,
+			backgroundColor: gradientStroke,
+			borderColor: '#ff8d72',
+			borderWidth: 2,
+			borderDash: [],
+			borderDashOffset: 0.0,
+			pointBackgroundColor: '#ff8d72',
+			pointBorderColor: 'rgba(255,255,255,0)',
+			pointHoverBackgroundColor: '#d048b6',
+			pointBorderWidth: 20,
+			pointHoverRadius: 4,
+			pointHoverBorderWidth: 15,
+			pointRadius: 4,
+			data: [500, 905, 300, 1500, 400, 1203, 1400, 350, 700, 1400, 900, 1500] //CHART DATA
+		}]
+	};
+
+	var myChart = new Chart(ctx, {
+		type: 'line',
+		data: data,
+		options: gradientChartOptionsConfigurationWithTooltipOrange //CHART FORMAT
+	});
+
+}
+
+
+
+
+demo.initDashboardPageCharts();
